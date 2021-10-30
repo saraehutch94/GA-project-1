6f4d377adba2f52105934447cc3ad773252edba1
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-30 15:52:03 -0400

    added comments to JS

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Constants
 
+// API base url variable
 const BASE_URL = "https://gateway.marvel.com";
+
+// API key variable
 const API_KEY = "bc26cb9df88a2b0baf8aff36d5e14665";
 
 // Variables
 
+// global data variable
 let marvelData;
 
 // Cached Element References
@@ -17,10 +21,19 @@ const $storyTitle = $(".story-title");
 
 // Event Listeners
 
+// event listener on form element, listens for user submit
 $form.on("submit", handleSubmit);
 
 // Functions
 
+// handleSubmit function:
+// selects event object
+// prevents form from refreshing page
+// takes input value and sets to variable
+// jQuery AJAX method + API call + API Key -> request Marvel's API using API key
+// successful request: returns promise object (data)
+// take data from API and set it to global variable "marvelData" -> can now use anywhere in code
+// render function called -> presents data in the DOM
 function handleSubmit(evt) {
     evt.preventDefault();
     const characterName = $input.val();
@@ -34,6 +47,10 @@ function handleSubmit(evt) {
     });
 };
 
+// render function:
+// enter into data object and access name, description, and image of character
+// assign each of those values to a variable
+// render these variables to the DOM using cached element references + interpolation
 function render() {
     const name = marvelData.data.results[0].name;
     const description = marvelData.data.results[0].description;
@@ -47,11 +64,14 @@ function render() {
             <img src="${imageURL}" class="image" alt="Image of Marvel Character" width="325" height="325">
         </article>
     `);
+    // loop through event array and grab the name property within each element of the array
+    // append each name property from each element to the DOM
     const eventArray = marvelData.data.results[0].events.items;
     for (let i = 0; i < eventArray.length; i++) {
         let stories = eventArray[i].name;
         $storiesDiv.append(`<p>${stories}</p>`);
     }
+    // CSS edits to events div
     $storiesDiv.css({
         "display": "flex",
         "flex-wrap": "wrap",
@@ -59,21 +79,28 @@ function render() {
         "width": "550px",
         "margin": "0 0 40px 0"
     });
+    // CSS edits to "featured in" title
     $storyTitle.css({
         "display": "block",
         "margin": "-20px 0 10px 0"
     });
+    // add class to "featured in" title
     $storyTitle.addClass("story-title-font");
+    // CSS edits to main element
     $main.css({
         "display": "flex",
         "flex-wrap": "wrap",
         "justify-content": "center",
         "align-items": "center",
     });
+    // cache image elements from the API
+    // add circular border around those images
     const $image = $(".image");
     $image.css({
         "border-radius": "50%"
     });
+    // cache name of character and description article element
+    // CSS edits to cached article element
     const $nameAndDesc = $(".name-and-desc");
     $nameAndDesc.css({
         "font-size": "18px",
@@ -81,7 +108,10 @@ function render() {
         "margin": "0 0 20px 0",
         "padding": "10px"
     });
+    // cache name of character
+    // add class to "nameTitle"
     const $nameTitle = $(".name-title");
     $nameTitle.addClass("title-font");
+    // clear input area after user submits
     $input.val("");
 };
